b81118688272eed2b45222a455246ebef62ba9a9
Pankaj Das <kumardas67@gmail.com>
2023-10-09 15:19:44 +0530

    changes made tp the js else if

--- script.js
+++ script.js
@@ -11,7 +11,13 @@ function calculateBmi(){
     bmiInputEl.value = bmiValue;
     
     if(bmiValue < 18.5){
-        weightConditionEl.innerText = '     Under Weight'
+        weightConditionEl.innerText = ' Under Weight'
+    }else if(bmiValue >= 18.5 && bmiValue <= 24.9){
+        weightConditionEl.innerText = ' Normal Weight'
+    }else if(bmiValue >= 25 && bmiValue <= 29.9){
+        weightConditionEl.innerText = 'Over Weight'
+    }else if(bmiValue >= 30){
+        weightConditionEl.innerText = 'Obesity'
     }
 }
 
